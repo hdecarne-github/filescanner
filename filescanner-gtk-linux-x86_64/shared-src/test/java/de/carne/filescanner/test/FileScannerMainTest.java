c4fe4167a885da31e816ea17971e5c5033703e3a
Holger <hdecarne@gmail.com>
2019-01-01 14:22:22 +0100

    Update file comment

--- filescanner-gtk-linux-x86_64/shared-src/test/java/de/carne/filescanner/test/FileScannerMainTest.java
+++ filescanner-gtk-linux-x86_64/shared-src/test/java/de/carne/filescanner/test/FileScannerMainTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007-2018 Holger de Carne and contributors, All Rights Reserved.
+ * Copyright (c) 2007-2019 Holger de Carne and contributors, All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
