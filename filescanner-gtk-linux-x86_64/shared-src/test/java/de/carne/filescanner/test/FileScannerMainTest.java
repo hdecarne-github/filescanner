c7e90026560024dae875da6c95620c8ea4c9e7e5
Holger <hdecarne@gmail.com>
2019-01-13 23:40:38 +0100

    Fix test setup issue

--- filescanner-gtk-linux-x86_64/shared-src/test/java/de/carne/filescanner/test/FileScannerMainTest.java
+++ filescanner-gtk-linux-x86_64/shared-src/test/java/de/carne/filescanner/test/FileScannerMainTest.java
@@ -16,14 +16,28 @@
  */
 package de.carne.filescanner.test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Locale;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Properties;
 
+import org.eclipse.swt.widgets.ProgressBar;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import de.carne.boot.Application;
 import de.carne.filescanner.FileScannerMain;
+import de.carne.swt.platform.PlatformIntegration;
 import de.carne.test.swt.DisableIfThreadNotSWTCapable;
 import de.carne.test.swt.tester.SWTTest;
+import de.carne.test.swt.tester.accessor.ControlAccessor;
+import de.carne.test.swt.tester.accessor.ItemAccessor;
 
 /**
  * Test {@link FileScannerMain} class.
@@ -38,16 +52,58 @@ class FileScannerMainTest extends SWTTest {
 
 	@Test
 	void testFileScanner() {
-		Script script = script(new FileScannerMain());
+		Script script = script(Application::run).args("--debug");
 
-		script.add(this::doCloss);
+		script.add(this::doOpenFile);
+		script.add(this::waitScanFinished, this::doVerifyScanResult, 60 * 1000);
+		script.add(this::doClose);
 		script.execute();
 	}
 
-	private void doCloss() {
+	private void doOpenFile() {
+		traceAction();
+
+		Path file = Paths.get("./build/libs",
+				"filescanner-" + PlatformIntegration.toolkitName() + "-" + getFileScannerVersion() + ".jar");
+
+		mockFileDialog().result(file.toString());
+
+		accessShell().accessMenuBar().accessMenuItem(ItemAccessor.matchText("&Open\u2026")).select();
+	}
+
+	private ControlAccessor<ProgressBar> waitScanFinished() {
+		ControlAccessor<ProgressBar> progressBarAccessor = accessShell().accessChild(ControlAccessor::wrapControl,
+				ProgressBar.class, (control) -> true);
+		Optional<? extends ProgressBar> optionalProgressBar = progressBarAccessor.getOptional();
+
+		return (optionalProgressBar.isPresent() && isProgressBarMaxedOut(optionalProgressBar.get())
+				? progressBarAccessor
+				: new ControlAccessor<>(Optional.empty()));
+	}
+
+	private boolean isProgressBarMaxedOut(ProgressBar progressBar) {
+		return progressBar.getSelection() >= progressBar.getMaximum();
+	}
+
+	private void doVerifyScanResult(ControlAccessor<ProgressBar> progressBar) {
+
+	}
+
+	private void doClose() {
 		traceAction();
 
 		accessShell().get().close();
+	}
+
+	private String getFileScannerVersion() {
+		Properties properties = new Properties();
+
+		try (InputStream propertiesStream = new FileInputStream("../gradle.properties")) {
+			properties.load(propertiesStream);
+		} catch (IOException e) {
+			Assertions.fail(e);
+		}
+		return Objects.requireNonNull(properties.getProperty("version"));
 	}
 
 }
