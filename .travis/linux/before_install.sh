3fe400a648f65576bf153f57cd108845705403af
Holger <hdecarne@gmail.com>
2020-08-11 22:29:23 +0200

    Make CI script more generic

--- .travis/linux/before_install.sh
+++ .travis/linux/before_install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
 jabba ls-remote
 jabba install $BUILD_JDK
@@ -9,9 +7,4 @@ jabba use $BUILD_JDK
 export JAVA_HOME="$HOME/.jabba/jdk/$BUILD_JDK"
 export PATH="$JAVA_HOME/bin:$PATH"
 
-sudo -E apt-get -yq --no-install-suggests --no-install-recommends install \
-fluxbox \
-imagemagick \
-libwebkit2gtk-4.0-37
-
-set +x
+sudo -E apt-get -yq --no-install-suggests --no-install-recommends install fluxbox imagemagick libwebkit2gtk-4.0-37
